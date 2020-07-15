d534fa29ca95d1c81cf01d20d975346bab0adab9
sohuang <sophiexhuang@gmail.com>
2020-07-14 19:02:40 -0700

    Added lecture sidebar for Lecture1 page

--- javascriptStyle.js
+++ javascriptStyle.js
@@ -1,54 +1,73 @@
- function dropdownMenu() {
-        
-        var x = document.getElementById("dropdownClick");
-        if (x.className == "topnav") {
-            x.className += " responsive";
-        } else {
-            x.className = "topnav";
-        }
+/*eslint no-undef: "error"*/
+/*eslint-env browser*/
+
+function dropdownMenu() {
+    var x = document.getElementById("dropdownClick");
+    if (x.className == "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
     }
+}
         
 function dropdownMenuLecture() {
-        
-        var x = document.getElementById("dropdownClickLecture");
-        if (x.className == "topnavLecture") {
-            x.className += " responsive";
-        } else {
-            x.className = "topnavLecture";
-        }
+    var x = document.getElementById("dropdownClickLecture");
+    if (x.className == "topnavLecture") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnavLecture";
     }
+}
 
 function dropdownMenuQuestion() {
-        
-        var x = document.getElementById("dropdownClickQuestion");
-        if (x.className == "sidenavQuestion") {
-            x.className += " responsive";
-        } else {
-            x.className = "sidenavQuestion";
-        }
+    var x = document.getElementById("dropdownClickQuestion");
+    if (x.className == "sidenavQuestion") {
+        x.className += " responsive";
+    } else {
+        x.className = "sidenavQuestion";
     }
+}
         
 function questionCheck(answer, docId) {
     var x = document.getElementById(docId);
-    if (answer=="TRUE") {
+    if (answer == "TRUE") {
         x.style.backgroundColor = "#a9fca7";
     } else {
-            x.style.backgroundColor = "#ff7373";
-        }
+        x.style.backgroundColor = "#ff7373";
     }
+}
 
-function showSolution(docId){
+function showSolution(docId) {
     var x = document.getElementById(docId);
     if (x.className == "questionSolution") {
-            x.className += " visible"; 
-        } else {
-            x.className = "questionSolution";
-        }
+        x.className += " visible";
+    } else {
+        x.className = "questionSolution";
+    }
 }
         
-function hideSolution(docId){
+function hideSolution(docId) {
     var x = document.getElementById(docId);
-    if (x.style.display != "none"){
+    if (x.style.display != "none") {
         x.style.display == "none";
     }
 }
+
+
+/* sidebar from w3schools.com */
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("lectureSidebarExpanded").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    //document.getElementById("main").style.width = "calc(100% - 18px)";
+    document.getElementById("lectureSidebarCollapsed").style.visibility = "hidden";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("lectureSidebarExpanded").style.width = "0";
+    document.getElementById("lectureSidebarCollapsed").style.marginLeft = "0";
+    document.getElementById("lectureSidebarCollapsed").style.visibility = "visible";
+    document.getElementById("main").style.marginLeft = "0";
+}
+/* End sidebar code from w3schools.com */
