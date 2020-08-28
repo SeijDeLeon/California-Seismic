4205fc7c90a9f9f3f328c7c6df98a2110f630f0f
sohuang <sophiexhuang@gmail.com>
2020-08-28 04:26:46 -0700

    Added media queries for top nav and footer
    for mobile responsiveness; breakpoint at 900px

--- javascriptStyle.js
+++ javascriptStyle.js
@@ -9,6 +9,15 @@ function dropdownMenu() {
         x.className = "topnav";
     }
 }
+
+function toggleDropdown() {
+    var x = document.getElementById("dropdownClick");
+    if (x.className == "menu-nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "menu-nav";
+    }
+}
         
 function dropdownMenuLecture() {
     var x = document.getElementById("dropdownClickLecture");
