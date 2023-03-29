1a8db8ca8a4792517e0f5523954673c51614d184
SeijDeLeon <sdeleon@berkeley.edu>
2023-03-29 14:58:48 -0700

    added hero image and call to action text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,22 @@
+import hero from '../assets/hero.jpg'
+import '../App.css'
+import { NavLink } from 'react-router-dom'
 export default function Home() {
 
   return (
-    <h1>Home</h1>
+    <main>
+      <div className="hero">
+        <div className="absolute z-10 md:left-[10%] top-[40%]">
+          <h1 className="md:flex justify-start text-5xl md:text-7xl text-white font-bold mb-4">Study for Free</h1>
+          <p className="md:flex justify-start text-2xl text-white font-medium">You can pass the Seismic Exam. </p>
+            <p className="md:flex justify-start text-2xl text-white font-medium pb-8">Everything needed is right here</p>
+          <NavLink className="md:flex w-fit justify-start text-white text-lg font-bold bg-blue-600 p-3 rounded-lg shadow-lg" to='/practice'>
+            <span className="sr-only">button</span>
+            Start Practicing
+          </NavLink>
+        </div>
+      </div>
+    </main>
   );
 
 }
