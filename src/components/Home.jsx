4d2fc586372e69c4ea425acd786722f4d5a998e0
SeijDeLeon <sdeleon@berkeley.edu>
2023-03-29 19:43:54 -0700

    add styling and Navlinks to card

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,11 @@ import hero from '../assets/hero.jpg'
 import '../App.css'
 import { NavLink } from 'react-router-dom'
 export default function Home() {
+  const cards = [
+    {link: '/lectures', title: '10+ Lectures', text:'Start your studies here', img:'https://img.icons8.com/ios/100/null/podium-with-speaker.png', alt:"speaker at podium"},
+    {link: '/practice', title: '100+ Questions', text:'Test your seismic knowledge with practice questions', img:'https://img.icons8.com/ios/100/null/test--v1.png', alt:'paper test'},
+    {link: '/exam-guide', title: 'Exam Resources', text:'Be ready to take the Seismic Principles exam', img:'https://img.icons8.com/ios/100/null/faq.png', alt:'question answer bubbles'}
+  ]
 
   return (
     <main>
@@ -16,25 +21,19 @@ export default function Home() {
           </NavLink>
         </div>
       </div>
-      <article>
+      <article className="bg-slate-50 md:py-12">
         <div className="max-w-6xl m-auto">
-          <h1 className="font-bold text-5xl">Explore</h1>
-          <div className="flex flex-col md:flex-row justify-around">
-            <figure className="w-72 m-auto">
-              <img className="h-40 flex m-auto" alt="speaker at podium" src="https://img.icons8.com/ios/100/null/podium-with-speaker.png"/>
-              <h2 className="font-bold text-2xl">10+ Lectures</h2>
-              <p>Start your studies here</p>
-            </figure>
-            <figure className="w-72 m-auto">
-              <img className="h-40 flex m-auto" alt="paper test" src="https://img.icons8.com/ios/100/null/test--v1.png"/>
-              <h2 className="font-bold text-2xl">100+ Questions</h2>
-              <p>Test your seismic knowledge with practic questions</p>
-            </figure>
-            <figure className="w-72 m-auto">
-              <img className="h-40 flex m-auto" alt="faq speech bubbles" src="https://img.icons8.com/ios/100/null/faq.png"/>
-              <h2 className="font-bold text-2xl">Exam Resources</h2>
-              <p>Be ready to take the Seismic Principles exam</p>
-            </figure>
+          <h1 className="font-bold text-5xl mb-8 md:mb-12">Explore</h1>
+          <div className="flex flex-col md:flex-row">
+            {cards.map((item, index) => (
+              <NavLink className="w-72 m-auto my-4 ease-out duration-300 rounded-lg hover:shadow-lg hover:bg-white" to={item.link}>
+                <figure key={index} className="">
+                  <img className="h-28 md:h-40 flex m-auto pt-2" alt={item.alt} src={item.img}/>
+                  <h2 className="font-bold text-2xl">{item.title}</h2>
+                  <p>{item.text}</p>
+                </figure>
+              </NavLink>
+            ))}
           </div>
         </div>
       </article>
