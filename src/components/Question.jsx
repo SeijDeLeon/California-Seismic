d53ecb4491064dc4edff0f4a2517866426cd6826
SeijDeLeon <sdeleon@berkeley.edu>
2023-05-09 19:19:19 -0700

    add styling to problem in Practice

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -9,6 +9,8 @@ export default function Question( { question, title } ) {
 
   var randomizedAnswers = [];
 
+  const arrowChevronDown = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M12 17.414 3.293 8.707l1.414-1.414L12 14.586l7.293-7.293 1.414 1.414L12 17.414z"/></svg>;
+  const arrowChevronUp = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="m12 6.586-8.707 8.707 1.414 1.414L12 9.414l7.293 7.293 1.414-1.414L12 6.586z"/></svg>
   useEffect(()=> {
   }, []);
   const getRandomBoolean = () => {
@@ -43,33 +45,31 @@ export default function Question( { question, title } ) {
 
 
   return (
-    <div className='max-w-4xl bg-white border-solid border rounded-md m-auto'>
+    <div className='max-w-4xl px-16 bg-white border-solid border rounded-md m-auto py-4'>
       <div className='block'>Earthquakes</div>
       <div className='flex justify-center'>
         <p>Previous</p>
         <p>Next</p>
       </div>
-      <div className='flex max-w-md m-auto border-solid border' >
-        <p>Q</p>
-        <p>{question.question}</p>
-      </div>
-      <div className='block'>
+      <p className='flex max-w-md m-auto border-solid border'>{`Q: ${question.question}`}</p>
+      <div className='block py-4 m-auto'>
         {randomizedAnswers.map((answer, index) => {
           return (
-            <label key={index} className={`block text-left ${(answer===question.answer && solutionDisplay)? 'bg-yellow-200' : ''}`}>
-              <input type='radio' value={answer} onChange={handleChange} checked={checkedItem === answer}/> {answer}
+            <label key={index} className={`block text-left ${(answer===question.answer && solutionDisplay)? 'bg-gradient-to-r from-yellow-200' : ''}`}>
+              <input type='radio' value={answer} onChange={handleChange} checked={checkedItem === answer}/> &#160;{answer}
             </label>
           )
         })}
       </div>
-      <section >
-        <p className="cursor-pointer" onClick={toggleSolutionDisplay} >Solution</p>
+      <section className='bg-gradient-to-r from-blue-300 via-sky-200 to-sky-100 rounded m-auto shadow-inner'>
+        <p className='cursor-pointer' onClick={toggleSolutionDisplay} >Solution</p>
         {solutionDisplay ?
           <div>
             <p>{`Answer: ${question.answer}`}</p>
-            <p>{question.solution}</p>
+            <p className='text-left p-4'>{question.solution}</p>
           </div>
         : <></>}
+        <div className='fill-white hover:fill-black m-auto flex justify-center cursor-pointer' onClick={toggleSolutionDisplay}>{solutionDisplay ? arrowChevronUp : arrowChevronDown}</div>
       </section>
     </div>
 
