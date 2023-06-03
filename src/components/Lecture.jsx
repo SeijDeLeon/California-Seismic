9b1d5209720df3294ac3ecc6df68187ea03c8094
SeijDeLeon <sdeleon@berkeley.edu>
2023-06-03 11:19:22 -0700

    add both , navlinks, styling

--- src/components/Lecture.jsx
+++ src/components/Lecture.jsx
@@ -1,18 +1,17 @@
-import UnderConstruction from './UnderConstruction.jsx';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom'
 import '../App.css';
 
 export default function Lectures({ display=false }) {
-  //define stuff
-  const { id } = useParams();
-  const idInt = parseInt(id, 10) - 1;
+  const { id } = useParams(); //retrieve lecture id from url
+  const idInt = parseInt(id, 10) - 1; //array based integer corresponding to lecture
 
 
   const lectureData = [
     { title: '01: Intro to Seismic Exam',
       googleLink:'https://docs.google.com/document/d/e/2PACX-1vRXMDtgh4vUDHDi0YmJNOHZK_S6x-XqTingo_6fTYuu-rvtGR4IcrbAy5rb0SvDE4gzDlGIEfbdFcx7/pub?embedded=true',
-      references:['https://www.bpelsg.ca.gov/applicants/candidate_info.shtml'],
+      references:[['https://www.bpelsg.ca.gov/applicants/candidate_info.shtml','Exam Website'],['https://www.asce.org/publications-and-news/asce-7','ASCE 7'],['https://codes.iccsafe.org/content/CABC2022P1','CBC']],
       summary:'This lecture is an overview of the California Seismic Principles Exam. The basics of where the exam sits on the path to earning a PE license, and its requirements will be explained. Nothing in this lecture will be on the exam',
       prereqs:['none'],
       objectives:['Exam requirements and general information']},
@@ -116,9 +115,12 @@ export default function Lectures({ display=false }) {
       objectives:['Visually identify proper detailing procedures for common lateral resisting systems']},
   ];
 
-  const Sidebar = <aside className='text-left sticky top-32'><ul>
-    {lectureData.map((lecture) => <li className={(id===lecture.title.slice(0,2) ? 'bg-sky-100 font-bold py-2' : 'py-2')} key={lecture.title}>{lecture.title}</li>)}
-    </ul></aside>
+  const Sidebar = <aside className='text-left sticky top-0'>
+    <p className='text-lg text-left py-3 text-slate-500'>Lectures</p>
+      <ul>
+        {lectureData.map((lecture) => <li className={(id===lecture.title.slice(0,2) ? 'bg-sky-100 py-2' : 'text-slate-500 py-2')} key={lecture.title}> <NavLink to={`/lectures/${lecture.title.slice(0,2)}`}>{lecture.title}</NavLink> </li>)}
+      </ul>
+    </aside>
 
 useEffect(()=> {
   const target = document.getElementById('googleDoc');
@@ -132,26 +134,35 @@ useEffect(()=> {
 },[])
 
   return (
-    <main className='max-w-screen-xl m-auto'>
+    <main className='max-w-screen-2xl m-auto px-4'>
       <h1 className='text-center text-sky-800 text-3xl tracking-wide font-semibold'>{`Lecture ${lectureData[parseInt(id, 10)-1].title}`}</h1>
       <div className='flex h-full'>
-        <div className='sm:w-0 hidden lg:block lg:w-2/12 '>
+        <div className='sm:w-0 hidden lg:block lg:w-2/12'>
           {Sidebar}
         </div>
-        <section className='sm:w-full  lg:w-8/12 overflow-auto flex flex-col justify-center'>
+        <section className='sm:w-full  lg:w-8/12 overflow-auto flex flex-col justify-center px-4'>
+          <p className='text-sky-800 font-semibold text-lg text-left max-w-full border-b-2 border-r-0 border-t-0 border-l-0 border pt-4'>Video Lecture</p>
           <iframe className='m-auto mt-8' width="560" height="315" src="https://www.youtube.com/embed/_exd39bNjDY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-          <div className='max-w-lg m-auto'>
+          <p className='text-sky-800 font-semibold text-lg text-left max-w-full border-b-2 border-r-0 border-t-0 border-l-0 border pt-4'>Summary</p>
+          <div className='max-w-full m-auto bg-amber-50 px-4'>
             <h3>Lecture summary</h3>
             <p className='text-left pl-4'>{lectureData[idInt].summary}</p>
             <h3>Lecture Pre-Requisites</h3>
             <ul className='text-left pl-4 list-disc'>{lectureData[idInt].prereqs.map((prereq) => <li key={prereq}>{prereq}</li>)}</ul>
             <h3>After this lecture you will know</h3>
             <ul className='text-left pl-4 list-disc'>{lectureData[idInt].objectives.map((objective) => <li key={objective}>{objective}</li>)}</ul>
-
           </div>
+          <p className='text-sky-800 font-semibold text-lg text-left max-w-full border-b-2 border-r-0 border-t-0 border-l-0 border pt-4'>Written Lecture</p>
           <div className="w-full px-6" id='googleDoc'></div>
         </section>
-        <div className='sm:w-0 border hidden lg:flex lg:w-2/12'></div>
+        <div className='sm:w-0 hidden lg:block lg:w-2/12'>
+          <aside className='text-center sticky top-0'>
+            <p className='text-lg text-left py-4 text-slate-500'>References</p>
+              <ul className='text-left'>
+                {lectureData[idInt].references.map((reference, index)=> <li className='py-2 text-sky-700 underline' key={index}><a target='_blank' rel='noopener noreferrer' href={reference[0]}>{reference[1]}</a></li>)}
+              </ul>
+          </aside>
+        </div>
       </div>
     </main>
   )
