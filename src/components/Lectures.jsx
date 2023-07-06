198aca8cac0c252347697a7ba6ca52448a5bf5b5
Kanchan Mhatre <mhatrekanchan01@gmail.com>
2023-07-06 15:30:45 -0700

    second commit to Lectures.jsx
    created buttons with equal width and equal spacing

--- src/components/Lectures.jsx
+++ src/components/Lectures.jsx
@@ -191,18 +191,18 @@ export default function Lectures({ display=false }) {
           <div className="max-w-6xl px-5 mx-auto text-center">
             {/*Heading*/}
             <h2 className="text-xl font-bold text-center">Done with all the lectures?</h2>
-              <div className="flex flex-col md:flex-row md:space-x-6">
+              <div className="flex flex-col md:flex-row md:space-x-6 space-y-0">
                   {/*Button 1 */}
-                  <div className="flex-col items-center p-6 space-y-6 bg-white md:w-1/3">
-                    <Button text='Start Practicing' extraClasses="md:flex hover:bg-blue-800"/>
+                  <div className="flex flex-col items-center p-3 space-y-2 bg-white md:w-1/3">
+                    <Button text='Start Practicing' extraClasses="w-48 md:w-48 hover:bg-blue-800"/>
                   </div>
                   {/*Button 2 */}
-                  <div className="flex-col items-center p-6 space-y-6 bg-white md:flex md:w-1/3">
-                    <Button text='Go to Exam Guide' extraClasses="md:flex hover:bg-blue-800" link='/exam-guide'/>
+                  <div className="flex flex-col items-center p-3 space-y-2 bg-white md:flex md:w-1/3">
+                    <Button text='Go to Exam Guide' extraClasses="w-48 md:w-48 hover:bg-blue-800" link='/exam-guide'/>
                   </div>
                   {/*Button 3 */}
-                  <div className="flex-col items-center p-6 space-y-6 bg-white md:flex md:w-1/3">
-                    <Button text='Read FAQs' extraClasses="md:flex hover:bg-blue-800" link='/exam-guide'/>
+                  <div className="flex flex-col items-center p-3 space-y-2 bg-white md:flex md:w-1/3">
+                    <Button text='Read FAQs' extraClasses="w-48 md:w-48 hover:bg-blue-800" link='/exam-guide'/>
                   </div>
               </div>
           </div>
