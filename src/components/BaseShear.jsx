c492ee883d47a0f8ee3ceeaf8ed84d6747116d06
Christy Yuen <yuenchr1sty@gmail.com>
2023-07-10 16:49:32 -0700

    initial and playing with stuff

--- src/components/BaseShear.jsx
+++ src/components/BaseShear.jsx
@@ -8,32 +8,39 @@ const BaseShearDiagram = () => {
     drawChart();
   }, []);
 
+
   const drawChart = () => {
+    console.log('drawChart called');
     const forces = [
       { floor: 1, value: 100 }, // Example forces for each floor
-      { floor: 2, value: 200 },
+      { floor: 1, value: 200 }, // by having floor as 1, the rectangles stack
+      { floor: 1, value: 300 }, 
+      { floor: 1, value: 400 } 
     ];
 
     // Calculate the total base shear
-    const totalBaseShear = forces.reduce((sum, force) => sum + force.value, 0);
+    const totalBaseShear = 400; //Currently using this as the y-axis max
 
     // Set up SVG container using D3
     const svg = d3.select(chartRef.current)
       .append('svg')
-      .attr('width', '100%')
-      .attr('height', '100%');
+      .attr('width', '100%') //Uses the entire div the 100%
+      .attr('height', '100%')
+      .style('display', 'block') // SVG is displayed as a block element
+      .style('margin', '0 auto'); // Centering with margin
 
     // Set up scales and axes
     const xScale = d3.scaleBand()
       .domain(forces.map(force => force.floor))
-      .range([50, 450])
-      .padding(0.1);
+      .range([50, totalBaseShear])
+      .padding(0.3);
 
     const yScale = d3.scaleLinear()
       .domain([0, totalBaseShear])
       .range([250, 50]);
 
-    const xAxis = d3.axisBottom(xScale);
+    const xAxis = d3.axisBottom(xScale)
+      .tickFormat(d => `Base Shear`) // Custom label format 
     const yAxis = d3.axisLeft(yScale);
 
     // Draw the base shear diagram
@@ -45,7 +52,23 @@ const BaseShearDiagram = () => {
       .attr('x', force => xScale(force.floor))
       .attr('y', force => yScale(force.value))
       .attr('width', xScale.bandwidth())
-      .attr('height', force => 250 - yScale(force.value));
+      .attr('height', force => 250 - yScale(force.value))
+      .style("fill", "none") // Creating a outline rectangle
+      .style("stroke", "black") // Creating a outline rectangle
+      .style("stroke-width", "2px"); //Set the stroke to thicker
+
+    // Add labels to the rectangles
+    svg.selectAll('.label')
+      .data(forces)
+      .enter()
+      .append('text')
+      .attr('class', 'label')
+      .attr('x', force => xScale(force.floor) + xScale.bandwidth() / 2) // Position in the middle of the rectangle
+      .attr('y', force => yScale(force.value) +15) // y position(+15) to place it above the rectangle
+      .attr('text-anchor', 'middle') // Anchor the text in the middle
+      .text(force => force.value) // Use the value as the label text
+      .style("font-size", "12px")
+      .style("font-weight", "bold");
 
     // Append the axes to the SVG container
     svg.append('g')
@@ -54,13 +77,20 @@ const BaseShearDiagram = () => {
 
     svg.append('g')
       .attr('transform', 'translate(50, 0)')
-      .call(yAxis);
+      .call(yAxis)
+      .style("opacity", 0);;
   };
 
   return (
-    <div className="w-screen h-screen">
-      <div className="w-screen h-screen" ref={chartRef}></div>
-    </div>
+      <>
+      <div className="flex justify-center items-center">
+        <h1 className="text-center text-6xl text-black font-bold mb-4">Base Shear Diagram</h1>
+      </div>
+      <div className="justify-end flex items-center">
+        <div className="w-1/2 h-screen " ref={chartRef}></div>
+      </div>
+      
+    </>
   );
 };
 
