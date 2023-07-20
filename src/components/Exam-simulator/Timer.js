fc5ce20e9dae075cedd74b9dea6e081ab17e8e28
Kanchan Mhatre <mhatrekanchan01@gmail.com>
2023-07-19 19:48:36 -0700

    Update Timer.js
    Default timer set to 150 minutes

--- src/components/Exam-simulator/Timer.js
+++ src/components/Exam-simulator/Timer.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 function Timer() {
-  const [seconds, setSeconds] = useState(60);
+  const [seconds, setSeconds] = useState(9000);
 
   useEffect(() => {
     const interval = setInterval(() => {
