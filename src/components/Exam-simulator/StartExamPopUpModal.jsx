283864bb2eb89fa73e7367033d447e2bf9124476
Kanchan Mhatre <mhatrekanchan01@gmail.com>
2023-07-24 16:36:09 -0700

    Sixth commit
    Update to fix UI for Start exam pop up modal

--- src/components/Exam-simulator/StartExamPopUpModal.jsx
+++ src/components/Exam-simulator/StartExamPopUpModal.jsx
@@ -16,15 +16,16 @@ function StartExamPopUpModal({visible, onClose, selectedExam, index, onStartExam
     if(!visible) return null;
     return(
         <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center">
-        <div className="bg-white p-2 rounded-lg w-80 h-64 md:w-1/3 md:h-1/3">
-            <div className="max-w-6xl px-5 py-10 mx-auto mt-10 text-center">
-                <h1 className="text-5xl font-semibold text-center text-xl text-gray-700">
-                Do you want to begin?<br />
-                {selectedExam.name}
-                </h1>
-            </div>
-            {/* Buttons */}
-            <div className="text-xl flex justify-center px-5 py-5">
+            <div className="bg-white p-4 rounded-lg w-4/5 md:w-1/3 h-auto">
+                <div className="max-w-4xl px-5 py-10 mx-auto mt-10 text-center">
+                    <h1 className="text-2xl md:text-3xl font-semibold text-center text-gray-700">
+                        Do you want to begin?
+                        <br />
+                        {selectedExam.name}
+                    </h1>
+                </div>
+                {/* Buttons */}
+                <div className="text-xl flex justify-center px-5 py-5">
                     <button className="px-4 py-2 mr-2 bg-blue-500 text-white rounded-full" onClick={onClose}>
                         Go Back
                     </button>
