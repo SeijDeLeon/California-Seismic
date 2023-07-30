7dffae88f914a6db9830533b9182657739119409
Christy Yuen <yuenchr1sty@gmail.com>
2023-07-30 13:52:12 -0700

    made arrows?

--- src/components/BaseShear.jsx
+++ src/components/BaseShear.jsx
@@ -18,6 +18,10 @@ const BaseShearDiagram = () => {
     setUserInput(event.target.value);
   };
 
+  //thinking about using a for loop to populate the forces for the floors. 
+
+  const arrowSize = 10; // Size of the arrow
+
   const drawChart = () => {
     console.log('drawChart called');
     const forces = [
@@ -28,30 +32,34 @@ const BaseShearDiagram = () => {
     ];
 
     // Calculate the total base shear
-    const totalBaseShear = 400; //Currently using this as the y-axis max
+    //const totalBaseShear = 400; //Currently using this as the y-axis max
+    const totalBaseShear = d3.max(forces, force => force.value);
 
         // Set up scales and axes
         const xScale = d3.scaleBand()
-        .domain(forces.map(force => force.floor))
+        .domain(forces.map(force => force.floor)) 
         .range([50, totalBaseShear])
         .padding(0.3);
-  
+
       const yScale = d3.scaleLinear()
-        .domain([0, totalBaseShear])
+        .domain([0, totalBaseShear + arrowSize])
         .range([250, 50]);
   
       const xAxis = d3.axisBottom(xScale)
         .tickFormat(d => `Base Shear`) // Custom label format 
       const yAxis = d3.axisLeft(yScale);
 
+      const svgWidth = totalBaseShear + 100; // Adding extra space for better visualization
+      const svgHeight = 300; // Set an appropriate height
+
       // Set up SVG container using D3
-      let svg = d3.select(chartRef.current).select('svg');
+      let svg = d3.select(chartRef.current).select('svg'); //might hardcode this
       if (svg.empty()) {
         svg = d3
           .select(chartRef.current)
           .append('svg')
-          .attr('width', totalBaseShear)//Uses the entire div the 100%
-          .attr('height', totalBaseShear)
+          .attr('width', svgWidth)//Uses the entire div the 100%
+          .attr('height', svgHeight)
           .style('display', 'block')// SVG is displayed as a block element
           .style('margin', '0 auto');// Centering with margin
       } else {
@@ -72,18 +80,45 @@ const BaseShearDiagram = () => {
       .style("stroke", "black") // Creating a outline rectangle
       .style("stroke-width", "2px"); //Set the stroke to thicker
 
-    // Add labels to the rectangles
-    svg.selectAll('.label')
+    // // Add labels to the rectangles
+    // svg.selectAll('.label')
+    //   .data(forces)
+    //   .enter()
+    //   .append('text')
+    //   .attr('class', 'label')
+    //   .attr('x', force => xScale(force.floor) + xScale.bandwidth() / 2) // Position in the middle of the rectangle
+    //   .attr('y', force => yScale(force.value) +15) // y position(+15) to place it above the rectangle
+    //   .attr('text-anchor', 'middle') // Anchor the text in the middle
+    //   .text(force => force.value) // Use the value as the label text
+    //   .style("font-size", "12px")
+    //   .style("font-weight", "bold");
+
+      // Sample data for the bar graph
+    const barData = [10, 20, 30, 40, 50];
+
+    // Sample data for vertical forces (base shear)
+    const baseShearData = [15, 25, 10, 35, 30];
+
+    // Create a separate x scale for the arrows
+    const xArrowScale = d3.scaleLinear()
+     .domain([0, forces.length - 1]) // Using sequential indices (0, 1, 2, ...)
+     .range([50, totalBaseShear]);
+
+    // Add arrows pointing to the rectangles
+    svg.selectAll(".arrow")
       .data(forces)
       .enter()
-      .append('text')
-      .attr('class', 'label')
-      .attr('x', force => xScale(force.floor) + xScale.bandwidth() / 2) // Position in the middle of the rectangle
-      .attr('y', force => yScale(force.value) +15) // y position(+15) to place it above the rectangle
-      .attr('text-anchor', 'middle') // Anchor the text in the middle
-      .text(force => force.value) // Use the value as the label text
-      .style("font-size", "12px")
-      .style("font-weight", "bold");
+      .append("path")
+      .attr("class", "arrow")
+      .attr("d", (d, i) => {
+        const barX = xArrowScale(i) + xScale.bandwidth() / 2;
+        const barY = yScale(d.value);
+        const arrowX = barX;
+        const arrowY = barY - arrowSize; // Point the arrowhead to the top of the rectangle
+        return `M ${barX} ${barY} L ${arrowX - arrowSize / 2} ${arrowY} M ${barX} ${barY} L ${arrowX + arrowSize / 2} ${arrowY}`;
+      })
+      .attr("stroke", "blue") // Color of the arrow lines
+      .attr("stroke-width", 2); // Width of the arrow lines
 
     // Append the axes to the SVG container
     svg.append('g')
@@ -94,6 +129,7 @@ const BaseShearDiagram = () => {
       .attr('transform', 'translate(50, 0)')
       .call(yAxis)
       .style("opacity", 0);;
+
   };
 
   return (
