ae3906af07a3849b30187f9d5ae74e5ae4fe56e7
Joel Zapana <joel.zapana@sjsu.edu>
2024-06-21 14:58:45 -0700

    Chapter 1 info

--- src/assets/data/ASCE7/CH1/sections.js
+++ src/assets/data/ASCE7/CH1/sections.js
@@ -1,13 +1,128 @@
 const sections = {
   '1.1' : {
     title: 'Scope',
-    imgs: ['1.1.png'],
+    imgs: ['1.1.png']
+  },
+  '1.2' : {
+    title: 'Definitions and Symbols',
     subsections: {
       '1.2.1': {
         title: 'Definitions',
-        imgs: ['1.2.1_1.png, 1.2.1_2.png']
+        imgs: ['1.2.1_1.png, 1.2.1_2.png, 1.2.1_3.png']
+      },
+      '1.2.2': {
+        title: 'Symbols',
+        imgs: ['1.2.2.png']
       }
     }
+  },
+  '1.3' : {
+    title: 'Basic Requirements',
+    subsections: {
+      '1.3.1': {
+        title: 'Strength and Stiffness',
+        imgs: ['1.3.1_1.png, 1.3.1_2.png, 1.3.1_3.png, 1.3.1_4.png, 1.3.1_5.png, 1.3.1_6.png']
+      },
+      'table1.3-1': {
+        title: 'Target Reliability and Associated Reliability Indices for Load Conditions That Do Not Include Earthquake, Tsunami, or Extraordinary Events',
+        imgs: ['table1.3-1.png']
+      },
+      'table1.3-2': {
+        title: 'Target Reliability (Conditional Probability of Failure) for Structural Stability Caused by Earthquake',
+        imgs: ['table1.3-2.png']
+      },
+      'table1.3-3': {
+        title: 'Target Reliability (Conditional Probability of Failure) for Ordinary Noncritical Structural Members Caused by Earthquake',
+        imgs: ['table1.3-3.png']
+      },
+      '1.3.2': {
+        title: 'Serviceability',
+        imgs: ['1.3.2.png']
+      },
+      '1.3.3': {
+        title: 'Functionality',
+        imgs: ['1.3.3.png']
+      },
+      '1.3.4': {
+        title: 'Self-Straining Forces and Effects',
+        imgs: ['1.3.4.png']
+      },
+      '1.3.5': {
+        title: 'Analysis',
+        imgs: ['1.3.5.png']
+      },
+      '1.3.6': {
+        title: 'Counteracting Structural Actions',
+        imgs: ['1.3.6.png']
+      },
+      '1.3.7': {
+        title: 'Fire Resistance',
+        imgs: ['1.3.7.png']
+      }
+    }
+  },
+  '1.4' : {
+    title: 'General Structural Integrity',
+    imgs: ['1.4_1.png, 1.4_2.png'],
+    subsections: {
+      '1.4.1': {
+        title: 'Load Path Connections',
+        imgs: ['1.4.1.png']
+      },
+      '1.4.2': {
+        title: 'Lateral Forces',
+        imgs: ['1.4.2.png']
+      },
+      '1.4.3': {
+        title: 'Connection to Supports',
+        imgs: ['1.4.3.png']
+      },
+      '1.4.4': {
+        title: 'Anchorage of Structural Walls',
+        imgs: ['1.4.4.png']
+      },
+      '1.4.5': {
+        title: 'Extraordinary Loads and Events',
+        imgs: ['1.4.5.png']
+      }
+    }
+  },
+  '1.5' : {
+    title: 'Classification of Buildings and Other Structures',
+    subsections: {
+      '1.5.1': {
+        title: 'Risk Categorization',
+        imgs: ['1.5.1.png']
+      },
+      'table1.5-1': {
+        title: 'Risk Category of Buildings and Other Structures for Flood, Wind, Snow, Earthquake, and Ice Loads',
+        imgs: ['table1.5-1_1.png, table1.5-1_2.png']
+      },
+      'table1.5-2': {
+        title: 'Importance Factors by Risk Category of Buildings and Other Structures for Snow, Ice, and Earthquake Loads',
+        imgs: ['table1.5-2.png']
+      },
+      '1.5.2': {
+        title: 'Multiple Risk Categories',
+        imgs: ['1.5.2.png']
+      },
+      '1.5.3': {
+        title: 'Toxic, Highly Toxic, and Explosive Substances',
+        imgs: ['1.5.3_1.png, 1.5.3_2.png']
+      }
+    }
+  },
+  '1.6': {
+    title: 'Additions and Alterations to Existing Structures',
+    imgs: ['1.6.png']
+  },
+  '1.7': {
+    title: 'Load Tests',
+    imgs: ['1.7.png']
+  },
+  '1.8': {
+    title: 'Consensus Standards and other Referenced Documents',
+    imgs: ['1.8.png']
   }
 };
 
