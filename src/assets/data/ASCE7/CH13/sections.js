4736d8687bbc2ccd91bdccc1e66c15f82c0a363d
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-06-23 12:54:49 -0700

    Ch 13 info

---
+++ src/assets/data/ASCE7/CH13/sections.js
@@ -0,0 +1,248 @@
+const sections = {
+  '13.1': {
+    title: "General",
+    imgs: [],
+    subsections: {
+      '13.1.1': {
+        title: 'Scope',
+        imgs: ['13.1.1.png']
+      },
+      '13.1.2': {
+        title: 'Seismic Design Category',
+        imgs: ['13.1.2.png']
+      },
+      '13.1.3': {
+        title: 'Component Importance Factor',
+        imgs: ['13.1.3.png']
+      },
+      '13.1.4': {
+        title: 'Exemptions',
+        imgs: ['13.1.4_1.png', '13.1.4_2.png']
+      },
+      '13.1.5': {
+        title: 'Premanufactured Modular Mechanical and Electrical Systems',
+        imgs: ['13.1.5.png']
+      },
+      '13.1.6': {
+        title: 'Application of Nonstructural Component Requirements to Nonbuilding Structures',
+        imgs: ['13.1.6.png']
+      },
+      '13.1.7': {
+        title: 'Reference Documents',
+        imgs: ['13.1.7_1.png', '13.1.7_2.png']
+      },
+      '13.1.8': {
+        title: 'Reference Documents Using Allowable Stress Design',
+        imgs: ['13.1.8.png']
+      },
+    },
+  },
+  '13.2': {
+    title: "General Design Requirements",
+    imgs: [],
+    subsections: {
+      'table13.2-1' : {
+        title: 'Applicable Requirements for Architectural, Mechanical, and Electrical Components: Supports and Attachments',
+        imgs: ['table13.2-1.png']
+      },
+      '13.2.1': {
+        title: 'Applicable Requirements for Architectural, Mechanical, and Electrical Components, Supports, and Attachments',
+        imgs: ['13.2.1.png']
+      },
+      '13.2.2': {
+        title: 'Special Certification Requirements for Designated Seismic Systems',
+        imgs: ['13.2.2_1.png', '13.2.2_2.png']
+      },
+      '13.2.3': {
+        title: 'Consequential Damage',
+        imgs: ['13.2.3.png', '13.2.3.1.png']
+      },
+      '13.2.4': {
+        title: 'Flexibility',
+        imgs: ['13.2.4.png']
+      },
+      '13.2.5': {
+        title: 'Testing Alternative for Seismic Capacity Determination',
+        imgs: ['13.2.5_1.png', '13.2.5_2.png']
+      },
+      '13.2.6': {
+        title: 'Experience Data Alternative for Seismic Capacity Determination',
+        imgs: ['13.2.6.png']
+      },
+      '13.2.7': {
+        title: 'Construction Documents',
+        imgs: ['13.2.7.png']
+      },
+    },
+  },
+  '13.3': {
+    title: "Seismic Demands on Nonstructural Components",
+    imgs: [],
+    subsections: {
+      '13.3.1': {
+        title: 'Seismic Design Force',
+        imgs: ['13.3.1.1_1.png', '13.3.1.1_2.png', '13.3.1.2.png', '13.3.1.3.png', '13.3.1.4_1.png', '13.3.1.4_2.png', '13.3.1.4_3.png']
+      },
+      '13.3.2': {
+        title: 'Seismic Relative Displacements',
+        imgs: ['13.3.2.png', '13.3.2.1_1.png', '13.3.2.1_2.png', '13.3.2.2_1.png', '13.3.2.2_2.png']
+      },
+      '13.3.3': {
+        title: 'Component Period',
+        imgs: ['13.3.3_1.png', '13.3.3_2.png']
+      },
+    }
+  },
+  '13.4': {
+    title: "Nonstructural Component Anchorage",
+    imgs: ["13.4.png"],
+    subsections: {
+      '13.4.1': {
+        title: 'Design Force in the Attachment',
+        imgs: ['13.4.1.png']
+      },
+      '13.4.2': {
+        title: 'Anchors in Concrete or Masonry',
+        imgs: ['13.4.2.1.png', '13.4.2.2.png', '13.4.2.3.png']
+      },
+      '13.4.3': {
+        title: 'Installation Conditions',
+        imgs: ['13.4.3.png']
+      },
+      '13.4.4': {
+        title: 'Multiple Attachments',
+        imgs: ['13.4.4_1.png', '13.4.4_2.png']
+      },
+      '13.4.5': {
+        title: 'Power-Actuated Fasteners',
+        imgs: ['13.4.5.png']
+      },
+      '13.4.6': {
+        title: 'Friction Clips',
+        imgs: ['13.4.6.png']
+      },
+    },
+  },
+  '13.5': {
+    title: "Architectual Components",
+    imgs: [],
+    subsections: {
+      'table13.5-1': {
+        title: 'Coefficients for Architectural Components',
+        imgs: ['table13.5-1_1.png', 'table13.5-1_2.png'],
+      },
+      'table13.5-2': {
+        title: 'Reference Standards for Structural Sealant Glazing',
+        imgs: ['table13.5-2.png'],
+      },
+      '13.5.1': {
+        title: 'General',
+        imgs: ['13.5.1.png']
+      },
+      '13.5.2': {
+        title: 'Forces and Displacements',
+        imgs: ['13.5.2.png']
+      },
+      '13.5.3': {
+        title: 'Exterior Nonstructural Wall Elements and Connections',
+        imgs: ['13.5.3_1.png', '13.5.3_2.png', '13.5.3_3.png', '13.5.3_4.png']
+      },
+      '13.5.4': {
+        title: 'Glass',
+        imgs: ['13.5.4.png',]
+      },
+      '13.5.5': {
+        title: 'Out-of-Plane Bending',
+        imgs: ['13.5.5_1.png', '13.5.5_2.png']
+      },
+      '13.5.6': {
+        title: 'Suspended Ceilings',
+        imgs: ['13.5.6.png', '13.5.6.1.png', '13.5.6.2_1.png', '13.5.6.2_2.png', '13.5.6.3.png']
+      },
+      '13.5.7': {
+        title: 'Access Floors',
+        imgs: ['13.5.7.1.png', '13.5.7.2_1.png', '13.5.7.2_2.png']
+      },
+      '13.5.8': {
+        title: 'Partitions',
+        imgs: ['13.5.8.1.png', '13.5.8.2.png']
+      },
+      '13.5.9': {
+        title: 'Glass in Glazed Curtain Walls, Glazed Storefronts, and Glazed Partitions',
+        imgs: ['13.5.9.1_1.png', '13.5.9.1_2.png', '13.5.9.2.png']
+      },
+      '13.5.10': {
+        title: 'Egress Stairs and Ramps',
+        imgs: ['13.5.10_1.png', '13.5.10_2.png', '13.5.10_3.png']
+      },
+    },
+  },
+  '13.6': {
+    title: "Mechanical and Electrical Components",
+    imgs: [],
+    subsections: {
+      'table13.6-1': {
+        title: 'Seismic Coefficients for Mechanical and Electrical Components',
+        imgs: ['table13.6-1_1.png', 'table13.6-1_2.png']
+      },
+      '13.6.1': {
+        title: 'General',
+        imgs: ['13.6.1_1.png', '13.6.1_2.png', '13.6.1_3.png']
+      },
+      '13.6.2': {
+        title: 'Mechanical Components',
+        imgs: ['13.6.2.png', '13.6.2.1.png']
+      },
+      '13.6.3': {
+        title: 'Electrical Components',
+        imgs: ['13.6.3_1.png', '13.6.3_2.png']
+      },
+      '13.6.4': {
+        title: 'Component Supports',
+        imgs: ['13.6.4.png', '13.6.4.1.png', '13.6.4.2.png', '13.6.4.3.png', '13.6.4.4_1.png', '13.6.4.4_2.png', '13.6.4.5.png',]
+      },
+      '13.6.5': {
+        title: 'Distribution Systems: Conduit, Cable Tray, and Raceways',
+        imgs: ['13.6.5_1.png', '13.6.5_2.png']
+      },
+      '13.6.6': {
+        title: 'Distribution Systems: Duct Systems',
+        imgs: ['13.6.6_1.png', '13.6.6_2.png', '13.6.6_3.png' ]
+      },
+      '13.6.7': {
+        title: 'Distribution Systems: Piping and Tubing Systems',
+        imgs: ['13.6.7_1.png', '13.6.7_2.png', '13.6.7.1.png', '13.6.7.2_1.png', '13.6.7.2_2.png', '13.6.7.3_1.png', '13.6.7.3_2.png', '13.6.7.3_3.png']
+      },
+      '13.6.8': {
+        title: 'Distribution Systems: Trapezes with a Combination of Systems',
+        imgs: ['13.6.8.png']
+      },
+      '13.6.9': {
+        title: 'Utility and Service Lines',
+        imgs: ['13.6.9.png']
+      },
+      '13.6.10': {
+        title: 'Boilers and Pressure Vessels',
+        imgs: ['13.6.10.png']
+      },
+      '13.6.11': {
+        title: 'Elevator and Escalator Design Requirements',
+        imgs: ['13.6.11.png', '13.6.11.1.png', '13.6.11.2.png', '13.6.11.3_1.png', '13.6.11.3_2.png', '13.6.11.4.png']
+      },
+      '13.6.12': {
+        title: 'Rooftop Solar Panels',
+        imgs: ['13.6.12_1.png', '13.6.12_2.png', '13.6.12_3.png']
+      },
+      '13.6.13': {
+        title: 'Other Mechanical and Electrical Components',
+        imgs: ['13.6.13_1.png', '13.6.13_2.png']
+      },
+    },
+  },
+  '13.7': {
+    title: "Consensus Standards and Other Referenced Documents",
+    imgs: ['13.7.png'],
+  }
+};
+
+export { sections };
