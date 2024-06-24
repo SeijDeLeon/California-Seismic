11d6b0809c1991b3746aaedf6f415b8c0f7b797b
Joel Zapana <joel.zapana@sjsu.edu>
2024-06-24 15:24:11 -0700

    Ch 2 info

---
+++ src/assets/data/ASCE7/CH2/sections.js
@@ -0,0 +1,101 @@
+const sections = {
+    '2.1' : {
+      title: 'General',
+      imgs: ['2.1.png']
+    },
+    '2.2' : {
+        title: 'Symbols',
+        imgs: ['2.2.png']
+      },
+    '2.3' : {
+        title: 'Load Combinations for Strength Design',
+        subsections: {
+            '2.3.1': {
+                title: 'Basic Combinations',
+                imgs: ['2.3.1_1.png, 2.3.1_2.png']
+            },
+            '2.3.2': {
+                title: 'Load Combinations Including Flood Load',
+                imgs: ['2.3.2.png']
+            },
+            '2.3.3': {
+                title: 'Load Combinations Including Atmospheric Ice Loads',
+                imgs: ['2.3.3.png']
+            },
+            '2.3.4': {
+                title: 'Load Combinations Including Self-Straining Forces and Effects',
+                imgs: ['2.3.4_1.png, 2.3.4_2.png']
+            },
+            '2.3.5': {
+                title: 'Load Combinations for Nonspecified Loads',
+                imgs: ['2.3.5.png']
+            },
+            '2.3.6': {
+                title: 'Basic Combinations with Seismic Load Effects',
+                imgs: ['2.3.6_1.png, 2.3.6_2.png']
+            }
+        }
+    },
+    '2.4' : {
+        title: 'Load Combinations for Allowable Stress Design',
+        subsections: {
+            '2.4.1': {
+                title: 'Basic Combinations',
+                imgs: ['2.4.1_1.png, 2.4.1_2.png, 2.4.1_3.png']
+            },
+            '2.4.2': {
+                title: 'Load Combinations Including Flood Load',
+                imgs: ['2.4.2.png']
+            },
+            '2.4.3': {
+                title: 'Load Combinations Including Atmospheric Ice Loads',
+                imgs: ['2.4.3_1.png, 2.4.3_2.png']
+            },
+            '2.4.4': {
+                title: 'Load Combinations Including Self-Straining Forces and Effects',
+                imgs: ['2.4.4.png']
+            },
+            '2.4.5': {
+                title: 'Basic Combinations with Seismic Load Effects',
+                imgs: ['2.4.5_1.png, 2.4.5_2.png']
+            }
+        }
+    },
+    '2.5' : {
+        title: 'Load Combinations for Extraordinary Events',
+        subsections: {
+            '2.5.1': {
+                title: 'Applicability',
+                imgs: ['2.5.1.png']
+            },
+            '2.5.2': {
+                title: 'Load Combinations',
+                imgs: ['2.5.2.png']
+            },
+            '2.5.3': {
+                title: 'Stability Requirements',
+                imgs: ['2.5.3.png']
+            }
+        }
+    },
+    '2.6' : {
+        title: 'Load Combinations for General Structural Integrity Loads',
+        imgs: ['2.6.png'],
+        subsections: {
+            '2.6.1': {
+                title: 'Strength Design Notional Load Combinations',
+                imgs: ['2.6.1.png']
+            },
+            '2.6.2': {
+                title: 'Allowable Stress Design Notional Load Combinations',
+                imgs: ['2.6.2.png']
+            }
+        }
+    },
+    '2.7' : {
+        title: 'Consensus Standards and Other Referenced Documents',
+        imgs: ['2.7.png']
+    }
+  };
+  
+  export { sections };
