6400a47916f2be642616dcab3d2d9258066e00b8
binhnguyen96html <binhnguyenbiz96@gmail.com>
2024-06-25 16:03:40 -0700

    change different colors for icons of dropdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        'deep-blue': '#14213D'
+        'deep-blue': '#14213D',
       },
     },
   },
   plugins: [],
-}
+  variants: {
+    textColor: ['group-hover', 'hover'],
+  },
+};
