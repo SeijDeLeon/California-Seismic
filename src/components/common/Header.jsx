8c90ccd146191ddcc29675a3e5fd8c16d857e6e0
binhnguyen96html <binhnguyenbiz96@gmail.com>
2024-06-25 17:16:35 -0700

    fix bugs in header: add function to close the anchor tag, edit routes for specific lecture

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -24,14 +24,12 @@ import { ChevronDownIcon } from '@heroicons/react/20/solid';
 import logo from '../../assets/images/logo.png';
 import { NavLink } from 'react-router-dom';
 
-
 // For hover-group for icons:
-// group-hover:text-blue-600 group-hover:text-amber-600 group-hover:text-teal-600 
-// group-hover:text-blue-500 group-hover:text-amber-500 group-hover:text-teal-500 
-//group-hover:text-blue-400 group-hover:text-amber-400 group-hover:text-teal-400 
-// group-hover:text-blue-300 group-hover:text-amber-300 group-hover:text-teal-300 
-// group-hover:text-blue-200 group-hover:text-amber-200 group-hover:text-teal-200 
-
+// group-hover:text-blue-600 group-hover:text-amber-600 group-hover:text-teal-600
+// group-hover:text-blue-500 group-hover:text-amber-500 group-hover:text-teal-500
+//group-hover:text-blue-400 group-hover:text-amber-400 group-hover:text-teal-400
+// group-hover:text-blue-300 group-hover:text-amber-300 group-hover:text-teal-300
+// group-hover:text-blue-200 group-hover:text-amber-200 group-hover:text-teal-200
 
 const products = [
   {
@@ -39,105 +37,105 @@ const products = [
     description: 'An overview of the exam and its requirements',
     href: '01',
     icon: AcademicCapIcon,
-    hoverColor: 'text-blue-600'
+    hoverColor: 'text-blue-600',
   },
   {
     name: '02: Geology & Earthquakes',
     description: 'Background information on how earthquakes occur',
     href: '02',
     icon: GlobeAmericasIcon,
-    hoverColor: 'text-amber-600'
+    hoverColor: 'text-amber-600',
   },
   {
     name: '03: Code Provisions',
     description: 'How the building code works',
     href: '03',
     icon: ScaleIcon,
-    hoverColor: 'text-teal-600'
+    hoverColor: 'text-teal-600',
   },
   {
     name: '04: Site Conditions',
     description: 'Seismic factors based on soil characteristics',
     href: '04',
     icon: MapPinIcon,
-    hoverColor: 'text-blue-500'
+    hoverColor: 'text-blue-500',
   },
   {
     name: '05: Risk Categories',
     description: 'Defining seismic risk by building type',
     href: '05',
     icon: UserGroupIcon,
-    hoverColor: 'text-amber-500'
+    hoverColor: 'text-amber-500',
   },
   {
     name: '06: Building Systems',
     description: 'Vertical and Lateral system',
     href: '06',
     icon: BuildingOffice2Icon,
-    hoverColor: 'text-teal-500'
+    hoverColor: 'text-teal-500',
   },
   {
     name: '07: Ductility',
     description: 'The concept of ductility and R values',
     href: '07',
     icon: HomeModernIcon,
-    hoverColor: 'text-blue-400'
+    hoverColor: 'text-blue-400',
   },
   {
     name: '08: Base Shear',
     description: 'How to calculate base shear',
     href: '08',
     icon: CalculatorIcon,
-    hoverColor: 'text-amber-400'
+    hoverColor: 'text-amber-400',
   },
   {
     name: '09: Force Distribution',
     description: 'How to determine story forces',
     href: '09',
     icon: ChartBarIcon,
-    hoverColor: 'text-teal-400'
+    hoverColor: 'text-teal-400',
   },
   {
     name: '10: Drift',
     description: 'Lateral drift calculations ',
     href: '10',
     icon: BuildingLibraryIcon,
-    hoverColor: 'text-blue-300'
+    hoverColor: 'text-blue-300',
   },
   {
     name: '11: Flexible Diaphragms',
     description: 'Intro to diaphragms and flexible analysis',
     href: '11',
     icon: HomeIcon,
-    hoverColor: 'text-amber-300'
+    hoverColor: 'text-amber-300',
   },
   {
     name: '12: Rigid Diaphragms',
     description: 'Rigid diaphgragm analysis',
     href: '12',
     icon: BuildingOfficeIcon,
-    hoverColor: 'text-teal-300'
+    hoverColor: 'text-teal-300',
   },
   {
     name: '13: Irregularities',
     description: 'Irregularities and redundancy as applied to seismic design',
     href: '13',
     icon: ExclamationTriangleIcon,
-    hoverColor: 'text-blue-200'
+    hoverColor: 'text-blue-200',
   },
   {
     name: '14: Non-structural Components',
     description: 'Forces and basic design of non-structural components',
     href: '14',
     icon: CubeIcon,
-    hoverColor: 'text-amber-200'
+    hoverColor: 'text-amber-200',
   },
   {
     name: '15: Seismic Detailing',
     description: 'How to identify good seismic load paths',
     href: '15',
     icon: PaintBrushIcon,
-    hoverColor: 'text-teal-200'
+    hoverColor: 'text-teal-200',
   },
 ];
 
@@ -311,11 +309,11 @@ export default function Header() {
                           />
                         </Disclosure.Button>
                         <Disclosure.Panel className="mt-2 space-y-2">
-                          {[...callsToAction, ...products].map((item) => (
+                          {[...callsToAction, ...products].map((item, index) => (
                             <Disclosure.Button
                               key={item.name}
                               as="a"
-                              href={item.href}
+                              href={`${index === 0 ? item.href : `lectures/${item.href}`}`}
                               className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                             >
                               {item.name}
@@ -328,18 +326,21 @@ export default function Header() {
                   <NavLink
                     className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                     to="/practice"
+                    onClick={() => setMobileMenuOpen(false)}
                   >
                     Practice
                   </NavLink>
                   <NavLink
                     className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                     to="/exam-guide"
+                    onClick={() => setMobileMenuOpen(false)}
                   >
                     Exam Guide
                   </NavLink>
                   <NavLink
                     className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                     to="/contributors"
+                    onClick={() => setMobileMenuOpen(false)}
                   >
                     Contributors
                   </NavLink>
