46531b3071376bd13e9c8ca565d785ae4ef0b52e
SeijDeLeon <sdeleon@berkeley.edu>
2024-06-27 17:48:52 -0700

    add ch11 images and section file

---
+++ src/assets/data/ASCE7/CH11/sections.js
@@ -0,0 +1,145 @@
+const sections = {
+  '11.1' : {
+    title: 'General',
+    subsections: {
+      '11.1.1': {
+        title: 'Purpose',
+        imgs: ['11.1.1.png']
+      },
+      '11.1.2': {
+        title: 'Scope',
+        imgs: ['11.1.2.png']
+      },
+      '11.1.3': {
+        title: 'Applicability',
+        imgs: ['11.1.3_1.png', '11.1.3_2.png']
+      },
+      '11.1.4': {
+        title: 'Alternate Materials and Methods of Construction',
+        imgs: ['11.1.4.png']
+      },
+      '11.1.5': {
+        title: 'Quality Assurance',
+        imgs: ['11.1.5']
+      },
+    }
+  },
+  '11.4' : {
+    title: 'Seismic Ground Motion Values',
+    subsections: {
+      '11.4.1': {
+        title: 'Near-Fault Sites',
+        imgs: ['11.4.1.png']
+      },
+      '11.4.2': {
+        title: 'Mapped Acceleration Parameters',
+        imgs: ['11.4.2_1.png', '11.4.2_2.png']
+      },
+      '11.4.3': {
+        title: 'Site Class',
+        imgs: ['11.4.3.png']
+      },
+      '11.4.4': {
+        title: 'Site Coefficients and Risk-Targeted Maximum Considered Earthquake Spectral Response Acceleration Parameters',
+        imgs: ['11.4.4.png', 'table11.4-1.png', 'table11.4-2.png', 'figure11.4-1.png']
+      },
+      'Figure 11.4-1': {
+        title: 'Design Responnse Spectrum',
+        imgs: ['figure11.4-1.png']
+      },
+      'Table 11.4-1': {
+        title: 'Short-Period Site Coefficient, Fa',
+        imgs: ['table11.4-1.png']
+      },
+      'Table 11.4-2': {
+        title: 'Long-Period Site Coefficient, Fv',
+        imgs: ['table11.4-2.png']
+      },
+      '11.4.5': {
+        title: 'Design Spectral Acceleration Parameters',
+        imgs: ['11.4.5.png']
+      },
+      '11.4.6': {
+        title: 'Design Response Spectrum',
+        imgs: ['11.4.6_1.png', '11.4.6_2.png']
+      },
+      '11.4.7': {
+        title: 'Risk-Targeted Maximum Considered Earthquake Response Spectrum',
+        imgs: ['11.4.7.png']
+      },
+      '11.4.8': {
+        title: 'Site-Specific Ground Motion Procedures',
+        imgs: ['11.4.8_1.png', '11.4.8_2.png']
+      },
+    }
+  },
+  '11.5' : {
+    title: 'Importance Factor And Risk Category',
+    subsections: {
+      '11.5.1': {
+        title: 'Importance Factor',
+        imgs: ['11.5.1.png']
+      },
+      '11.5.2': {
+        title: 'Protected Access for Risk Category IV',
+        imgs: ['11.5.2.png']
+      }
+    }
+  },
+  '11.6' : {
+    title: 'Importance Factor And Risk Category',
+    imgs: ['11.6_1.png', '11.6_2.png', 'table11.6-1.png', 'table11.6-2.png'],
+  },
+  '11.7' : {
+    title: 'Design Requirements for Seismic Design Category A',
+    imgs: ['11.7.png']
+  },
+  '11.8' : {
+    title: 'Geologic Hazards and Geotechnical Investigation',
+    subsections: {
+      '11.8.1': {
+        title: 'Site Limitation for Seismic Design Categories E and F',
+        imgs: ['11.8.1.png']
+      },
+      '11.8.2': {
+        title: 'Geotechnical Investigation Report Requirements for Seismic Design Categories C through F',
+        imgs: ['11.8.2_1.png', '11.8.2_2.png']
+      },
+      '11.8.3': {
+        title: 'Additional Geotechnical Investigation Report Requirements for Seismic Design Categories D through F',
+        imgs: ['11.8.3.png', 'table11.8-1.png']
+      },
+      'Table 11.8-1': {
+        title: 'Site Coefficient F_PGA',
+        imgs: ['table11.8-1.png']
+      }
+    }
+  },
+  '11.9' : {
+    title: 'Vertical Ground Motions for Seismic Design',
+    subsections: {
+      '11.9.1': {
+        title: 'General',
+        imgs: ['11.9.1.png']
+      },
+      '11.9.2': {
+        title: 'MCER Vertical Response Spectrum',
+        imgs: ['11.9.2_1.png', '11.9.2_2.png', 'table11.9-1.png']
+      },
+      'Table 11.9-1': {
+        title: 'Values of Vertical Coefficient Cv',
+        imgs: ['table11.9-1.png']
+      },
+      '11.9.3': {
+        title: 'Design Vertical Response Spectrum',
+        imgs: ['11.9.3.png']
+      },
+    }
+  },
+  '11.10' : {
+    title: 'Consensus Standards and Other Referenced Documents',
+    imgs: ['11.10.png']
+    }
+};
+
+export { sections };
