a6138e32989ce13b37df8c6534a7d5de24c9d83f
Joel Zapana <joel.zapana@sjsu.edu>
2024-07-09 12:56:06 -0700

    Fixed answer in question

--- src/components/features/ProblemGenerator/FundamentalPeriod.js
+++ src/components/features/ProblemGenerator/FundamentalPeriod.js
@@ -10,18 +10,38 @@ const shuffleArray = (array) => {
 const randomValGen = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 export const fundamentalPeriod = () => {
-  let height = randomValGen(8, 30);
-  let ksi = randomValGen(1, 29000);
+  let height = randomValGen(8, 30); //ft
+  //let ksi = randomValGen(1, 29000); //ksi = 1000 lb/in^2
+  let E = randomValGen(10000000, 29000000); //lb/in^2
+  let inertia = randomValGen(30000, 600000);
   let kips = randomValGen(5, 100);
-  let question = `Determine the fundamental period of the below SDOF structure. The
-          height is ${height} ft, the modulus of elasticity for the column is
-          ${ksi} ksi, and the mass at the top weighs ${kips} kips?`;
+  let pounds = kips * 1000;
+  let G = 386.09; //in/s^2
+  let questionText = `Determine the fundamental period of the below SDOF structure. The
+          height is \\(${height}\\ ft\\), the modulus of elasticity for the column is
+          \\(${E}\\ ksi\\), the moment of inertia is \\(${inertia}\\ in^{4}\\), and the mass at the top
+          weighs \\(${kips}\\ kips\\)?`;
+  let question = <MathJax>{questionText}</MathJax>
   let image = fundamentalPeriodImage;
+  let stiffness = parseFloat(
+    (
+      ((3 * E * inertia) / Math.pow(height * 12, 3))
+    ).toFixed(2)
+  );
+  /*
   let answer = parseFloat(
     (
       2 *
       Math.PI *
-      Math.sqrt((kips * Math.pow(height, 3)) / (ksi * 386.09))
+      Math.sqrt((kips * Math.pow(height, 3)) / (E * 386.09))
+    ).toFixed(2)
+  );
+  */
+  let answer = parseFloat(
+    (
+      2 *
+      Math.PI *
+      Math.sqrt((pounds) / (stiffness * G))
     ).toFixed(2)
   );
   let choices = shuffleArray([
@@ -36,13 +56,13 @@ export const fundamentalPeriod = () => {
   the period equation for an SDOF system. Determine stiffness: k =
   3(EI)/h3`;
 
-  let firstLine = `Provided Inputs: \\(h = ${height}\\ ft, W = ${kips}\\ kips, E = ${ksi}\\ ksi, I = XXX\\ in^{4}\\)`;
+  let firstLine = `Provided Inputs: \\(h = ${height}\\ ft, W = ${kips}\\ kips, E = ${E}\\ ksi, I = ${inertia}\\ in^{4}\\)`;
   let secondLine = "Constants: \\(G = 386.09\\ in/s^{2}\\)";
   let thirdLine = "Step 1: Determine the stiffness of the system (cantilevered column)";
-  let fourthLine = `\\(k = \\frac{3(EI)}{h^{3}} = \\frac{3(${ksi}\\ ksi)(XX\\ in^{4})}{(${height}\\ ft \\times 12\\ in/ft)^{3}} = XX\\ lb/in\\)`;
+  let fourthLine = `\\(k = \\frac{3(EI)}{h^{3}} = \\frac{3(${E}\\ ksi)(${inertia}\\ in^{4})}{(${height}\\ ft \\ \\times \\ 12\\ in/ft)^{3}} = ${stiffness}\\ lb/in\\)`;
   let fifthLine = "Step 2: Determine the fundamental period";
-  let sixthLine = `\\(T = 2\\pi\\sqrt{\\frac{m}{k}} = 2\\pi\\sqrt{\\frac{W}{Gk}} = 2\\pi\\sqrt{\\frac{X\\ lb}{(386.09\\ in/s^{2})(XX\\ lb/in)}} = ${answer}\\ s\\)`;
-  let solution = <MathJax>{sixthLine}</MathJax>;
+  let sixthLine = `\\(T = 2\\pi\\sqrt{\\frac{m}{k}} = 2\\pi\\sqrt{\\frac{W}{Gk}} = 2\\pi\\sqrt{\\frac{${pounds}\\ lb}{(386.09\\ in/s^{2})(${stiffness}\\ lb/in)}} = ${answer}\\ s\\)`;
+  let solution = <MathJax>{fourthLine} {sixthLine}</MathJax>;
 
   /*
   solution is expected to be a string, so this does not work:
