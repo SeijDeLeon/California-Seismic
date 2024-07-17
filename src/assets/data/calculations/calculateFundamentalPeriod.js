05d21917dd007674956021db3ed53801b89729ee
Joel Zapana <joel.zapana@sjsu.edu>
2024-07-16 17:38:56 -0700

    Fixed fundamental period calculate file again (replaced G with 386.09 since it should be fixed

--- src/assets/data/calculations/calculateFundamentalPeriod.js
+++ src/assets/data/calculations/calculateFundamentalPeriod.js
@@ -1,5 +1,5 @@
-const calculateFundamentalPeriod = (W, G, stiffness) => {
-    const fundamentalPeriod = 2 * Math.PI * Math.sqrt((W * 1000) / (G * stiffness));
+const calculateFundamentalPeriod = (W, stiffness) => {
+    const fundamentalPeriod = 2 * Math.PI * Math.sqrt((W * 1000) / (386.09 * stiffness));
 
     return fundamentalPeriod;
 };
