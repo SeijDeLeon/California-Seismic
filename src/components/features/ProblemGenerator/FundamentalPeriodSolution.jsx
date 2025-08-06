0ebcf96164a4275d8c7015645c460361c5528859
RoyCDev <rcheung0117@gmail.com>
2025-08-05 23:14:00 -0700

    removed br and unncessary p tags inside solutions

--- src/components/features/ProblemGenerator/FundamentalPeriodSolution.jsx
+++ src/components/features/ProblemGenerator/FundamentalPeriodSolution.jsx
@@ -9,45 +9,37 @@ export default function FundamentalPeriodSolution({ height, kips, E, inertia, G,
     */
     return (
         <Fragment>
-            <p className='flex justify-start'>
-                <MathJax>
-                    {`Provided Inputs:
+            <MathJax className='flex justify-self-start'>
+                {`Provided Inputs:
                     \\begin{align}
                         &h = ${height}\\ ft, \\\\
                         &W = ${kips}\\ kips,\\\\ 
                         &E = ${E}\\ ksi,\\\\ 
                         &I = ${inertia}\\ in^{4}
                     \\end{align}`}
-                </MathJax>
-            </p>
-            <p>
-                <MathJax>
-                    {`Constants: \\(G = ${G}\\ in/s^{2}\\)`}
-                </MathJax>
-            </p>
-            <br />
+            </MathJax>
+            <MathJax className='mb-6'>
+                {`Constants: \\(G = ${G}\\ in/s^{2}\\)`}
+            </MathJax>
+
             <p>Step 1: Determine the stiffness of the system (cantilevered column)</p>
-            <p className='flex justify-start'>
-                <MathJax>
-                    {`\\begin{align}
+            <MathJax className='flex justify-self-start mb-6'>
+                {`\\begin{align}
                         k &= \\frac{3(EI)}{h^{3}} \\\\[5px] 
                         &= \\frac{3(${E}\\ ksi)(${inertia}\\ in^{4})}{(${height}\\ ft \\ \\times \\ 12\\ in/ft)^{3}} \\\\[5px] 
                         &= ${stiffness}\\ lb/in
                     \\end{align}`}
-                </MathJax>
-            </p>
-            <br />
+            </MathJax>
+
             <p>Step 2: Determine the fundamental period</p>
-            <p className='flex justify-start'>
-                <MathJax>
-                    {`\\begin{align}
+            <MathJax className='flex justify-self-start'>
+                {`\\begin{align}
                         T &= 2\\pi\\sqrt{\\frac{m}{k}} \\\\[5px] 
                         &= 2\\pi\\sqrt{\\frac{W}{Gk}} \\\\[5px] 
                         &= 2\\pi\\sqrt{\\frac{${pounds}\\ lb}{(${G}\\ in/s^{2})(${stiffness}\\ lb/in)}} \\\\[5px] 
                         &= ${answer}\\ s
                     \\end{align}`}
-                </MathJax>
-            </p>
+            </MathJax>
         </Fragment>
     );
 }
--- src/components/features/ProblemGenerator/NonStructuralComponentSolution.jsx
+++ src/components/features/ProblemGenerator/NonStructuralComponentSolution.jsx
@@ -4,90 +4,68 @@ export default function NonStructuralComponentSolution({ equipmentName, table, t
     // Renamed Fp_original, Fp_max, and Fp_min.
     return (
         <Fragment>
-            <p>To determine the component force, use Section 13 of ASCE 7.</p>
-            <br />
+            <p className='mb-6'>To determine the component force, use Section 13 of ASCE 7.</p>
             <p>First determine the seismic coefficients for the components. Table {table} may be used for a/an {equipmentName}. The closest matching component is {tableName}, with the following values:</p>
-            <p>
-                <MathJax>
-                    {`\\(a_{p} = ${ap},\\ R_{p} = ${Rp}\\)`}
-                </MathJax>
-            </p>
-            <br />
+            <MathJax className='mb-6'>
+                {`\\(a_{p} = ${ap},\\ R_{p} = ${Rp}\\)`}
+            </MathJax>
+
             <p>Because there was no description for the component related to an increased importance factor, we can also assume a seismic importance factor:</p>
-            <p>
-                <MathJax>
-                    {`\\(I_{p} = ${Ip}\\)`}
-                </MathJax>
-            </p>
-            <br />
+            <MathJax className='mb-6'>
+                {`\\(I_{p} = ${Ip}\\)`}
+            </MathJax>
+
             <p>Second, determine the relative attachment point for the component relative to the building’s mean roof height. This is provided in the problem description:</p>
-            <p>
-                <MathJax>
-                    {`\\(z = ${z}\\ ft,\\ h = ${h}\\ ft\\)`}
-                </MathJax>
-            </p>
-            <br />
+            <MathJax className='mb-6'>
+                {`\\(z = ${z}\\ ft,\\ h = ${h}\\ ft\\)`}
+            </MathJax>
+
             <p>Third, use the equations 13.1-1, 13.3-2, and 13.3-3 to solve for the component force.</p>
-            <p className='flex justify-start'>
-                <MathJax>
-                    {`\\begin{align}
+            <MathJax className='flex justify-self-start mb-6'>
+                {`\\begin{align}
                         F_{p} &= \\frac{0.4a_{p}S_{DS}W_{p}}{(\\frac{R_{p}}{I_{p}})}(1 + 2\\frac{z}{h}) \\\\[5px]
                         &= \\frac{0.4(${ap})(${SDS})(${Wp})}{${Rp}}(1 + 2\\frac{${z}}{${h}}) \\\\[5px]
                         &= ${Fp_original}\\ lb
                     \\end{align}`}
-                </MathJax>
-            </p>
-            <br />
-            <p>
-                <MathJax>
-                    {`\\(F_{p}\\) is not required to be taken as greater than`}
-                </MathJax>
-            </p>
-            <p className='flex justify-start'>
-                <MathJax>
-                    {`\\begin{align}
+            </MathJax>
+
+            <MathJax>
+                {`\\(F_{p}\\) is not required to be taken as greater than`}
+            </MathJax>
+            <MathJax className='flex justify-self-start mb-6'>
+                {`\\begin{align}
                         F_{pmax} &= 1.6S_{DS}I_{p}W_{p} \\\\[5px]
                         &= 1.6(${SDS})(${Ip})(${Wp}) \\\\[5px]
                         &= ${Fp_max}\\ lb
                     \\end{align}`}
-                </MathJax>
-            </p>
-            <br />
-            <p>
-                <MathJax>
-                    {`and \\(F_{p}\\) shall not be taken as less than`}
-                </MathJax>
-            </p>
-            <p className='flex justify-start'>
-                <MathJax>
-                    {`\\begin{align}
+            </MathJax>
+
+            <MathJax>
+                {`and \\(F_{p}\\) shall not be taken as less than`}
+            </MathJax>
+            <MathJax className='flex justify-self-start mb-6'>
+                {`\\begin{align}
                         F_{pmin} &= 0.3S_{DS}I_{p}W_{p} \\\\[5px]
                         &= 0.3(${SDS})(${Ip})(${Wp}) \\\\[5px]
                         &= ${Fp_min}\\ lb
                     \\end{align}`}
-                </MathJax>
-            </p>
-            <br />
-            <p className='flex justify-start'>
-                <MathJax>
-                    {/* {`\\(\\eqalign{
+            </MathJax>
+
+            <MathJax className='flex justify-self-start mb-6'>
+                {/* {`\\(\\eqalign{
                         F_{p} &= max(min(F_{p},\\ F_{pmax}),\\ F_{pmin}) \\\\[5px]
                         &= max(min(${Fp_original}\\ lb,\\ ${Fp_max}\\ lb),\\ ${Fp_min}\\ lb) \\\\[5px]
                         &= ${answer}\\ lb
                     }\\)`} */}
-                    {`\\begin{align}
+                {`\\begin{align}
                         F_{p} &= max(min(F_{p},\\ F_{pmax}),\\ F_{pmin}) \\\\[5px]
                         &= max(min(${Fp_original}\\ lb,\\ ${Fp_max}\\ lb),\\ ${Fp_min}\\ lb) \\\\[5px]
                         &= ${answer}\\ lb
                     \\end{align}`}
-                </MathJax>
-            </p>
-            <br />
-            <p>
-                <MathJax>
-                    {`The seismic force on the component is \\(${answer}\\ lb\\).`}
-                </MathJax>
-            </p>
+            </MathJax>
+            <MathJax>
+                {`The seismic force on the component is \\(${answer}\\ lb\\).`}
+            </MathJax>
         </Fragment>
     );
 }
